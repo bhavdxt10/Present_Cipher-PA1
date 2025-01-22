28f8c03a31325ded8a938ce4c4fb4a9aea9f8d19
Manasi Ingle <210050093@iitb.ac.in>
2025-01-22 18:56:33 +0530

    Testbench file added

--- part-a/tb_sbox.v
+++ part-a/tb_sbox.v
@@ -1,11 +1,14 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
+
 module tb_sbox;
 
 // testbench signals
-reg [3:0] inp;
+reg [3:0] inp; 
 wire [3:0] outp;
+integer i;
 
 // instantiate the S-box module
+sbox uut ( .inp(inp), .outp(outp) );
 
 initial begin
     // display header
@@ -13,17 +16,21 @@ initial begin
     // monitor changes in inp and outp
     $monitor("%0dns\t %b\t %b", $time, inp, outp);
 
-    inp = 0;
     // stimulate the inp signal after a certain delay
     // test all possible values for inp signal
-
+    for (i = 0; i < 16; i = i + 1) begin
+        inp = i;
+        #10; 
+    end
+    
     // finish simulation
     $finish;
 end
 
+
 initial begin
     $dumpfile("sbox_dump.vcd");
-    $dumpvars(1, tb_sbox);
+    $dumpvars(1, tb_sbox);    
 end
 
 endmodule
